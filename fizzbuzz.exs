5a95f69f5dd5253a61b236b8d89d9de5ddbec239
Hector Garcia Rosales <ghectormus@gmail.com>
2023-02-18 15:05:04 -0600

    add: solution for page 45 part 2 functions calling functions

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -10,3 +10,13 @@ IO.puts fizzbuzz.(0,0,3)
 IO.puts fizzbuzz.(0,3,3)
 IO.puts fizzbuzz.(3,0,3)
 IO.puts fizzbuzz.(3,3,6)
+
+fizzbuzz_handler = fn n -> fizzbuzz.(rem(n,3),rem(n,5), n ) end
+
+IO.puts fizzbuzz_handler.(10)
+IO.puts fizzbuzz_handler.(11)
+IO.puts fizzbuzz_handler.(12)
+IO.puts fizzbuzz_handler.(13)
+IO.puts fizzbuzz_handler.(14)
+IO.puts fizzbuzz_handler.(15)
+IO.puts fizzbuzz_handler.(16)
