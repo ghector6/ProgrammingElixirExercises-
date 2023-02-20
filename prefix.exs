aef70336dacaa614e46eaaa4dc90d30c0f31a50c
Hector Garcia Rosales <ghectormus@gmail.com>
2023-02-20 14:37:18 -0600

    add: variable with two function calls

--- prefix.exs
+++ prefix.exs
@@ -3,3 +3,7 @@ prefix = fn string_1 -> (fn string_2 -> "#{string_1} #{string_2}" end ) end
 currently_learning = prefix.("I am currently learning: ")
 
 IO.puts currently_learning.("Elixir 🔮")
+
+fav_lang = currently_learning.("Elixir 🔮")
+
+IO.puts fav_lang
