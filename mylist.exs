e7a72e7c9d53b0fd08822292442cebe08e90656e
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-03 08:41:54 -0600

    add: creating a function reduce that recursevly add all values

--- mylist.exs
+++ mylist.exs
@@ -9,4 +9,9 @@ defmodule MyList do
 
   def map([], _func), do: []
   def map([ head | tail ], func ), do: [func.(head) | map(tail, func)]
+
+  def reduce([], value, _), do: value
+  def reduce([ head | tail ], value, func) do
+    reduce(tail, func.(head,value), func)
+  end
 end
