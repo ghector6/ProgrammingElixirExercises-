28fee81cca6e7adaa07b78bea9846ed9e4f5393e
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-10 15:15:35 -0600

    refactor: assigning each list to head

--- weather.exs
+++ weather.exs
@@ -5,10 +5,11 @@ defmodule WeatherHistory do
 
   #Generic location 
   def for_location([],_target_loc), do: []
-  def for_location([ [time, target_loc, temp, rain] | tail], target_loc) do
-    [[time, target_loc, temp, rain] | for_location(tail, target_loc)]
+  def for_location([ head=[_, target_loc, _, _] | tail], target_loc) do
+    [head | for_location(tail, target_loc)]
   end
   def for_location([_|tail], target_loc), do: for_location(tail,target_loc)
+
 
 
   def test_data do 
