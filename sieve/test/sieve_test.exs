ff18219633ba9c5db692c40c9683f6d385375130
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-01 15:14:48 -0600

    test: adding test for non integers values

--- sieve/test/sieve_test.exs
+++ sieve/test/sieve_test.exs
@@ -5,5 +5,10 @@ defmodule SieveTest do
     assert Sieve.up_to(20) == [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
   end
 
+  test "if it  receives a binary, converts to integerr" do
+    assert Sieve.up_to("20") == [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  end
+
+
 
 end
