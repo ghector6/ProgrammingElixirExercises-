2ac427a7f136747691051a42e35fcb2967d8ef26
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-01 15:18:02 -0600

    add: pattern match fn to convert to int
    Converts non integer values and call itself
    to pattern match the fn to handle

--- sieve/lib/sieve.ex
+++ sieve/lib/sieve.ex
@@ -2,6 +2,9 @@ defmodule Sieve do
   @moduledoc """
   Documentation for `Sieve`.
   """
+  def up_to(n) when is_binary(n) do
+    n |> String.to_integer() |> up_to()
+  end
 
   def up_to(n) do
     Enum.into 2..n, []
