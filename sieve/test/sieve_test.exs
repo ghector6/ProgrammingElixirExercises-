d4866efc996335e125664e7ebdddd702f7219f3c
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-01 15:05:27 -0600

    add: first test to create a range
    creates a list from 2 up to the given number

--- sieve/test/sieve_test.exs
+++ sieve/test/sieve_test.exs
@@ -1,8 +1,9 @@
 defmodule SieveTest do
   use ExUnit.Case
-  doctest Sieve
 
-  test "greets the world" do
-    assert Sieve.hello() == :world
+  test "returns a list from 2 up to the given range" do
+    assert Sieve.up_to(20) == [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
   end
+
+
 end
