6c38a3bb91fccd43defb79b0e878d7d375113817
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-02 20:07:00 -0600

    test: eliminate multiples of head
    still missing the square property

--- sieve/test/sieve_test.exs
+++ sieve/test/sieve_test.exs
@@ -9,6 +9,10 @@ defmodule SieveTest do
     assert Sieve.up_to("20") == [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
   end
 
+  test "returns the list of prime numbers from 2 to 20" do
+    assert Sieve.finder([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], []) == [2,3,5,7,11,13,17,19]
+  end
+
 
 
 end
