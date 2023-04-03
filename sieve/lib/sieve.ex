11010a6c59f97aa4e9738a1b5a2fba9bf9d0550a
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-02 20:08:34 -0600

    add: finder fn´s
    finding through pattern matching the prime numbers
    using comprenhesion to obtain elements form list
    Enum reverse to obtain correct order

--- sieve/lib/sieve.ex
+++ sieve/lib/sieve.ex
@@ -9,4 +9,14 @@ defmodule Sieve do
   def up_to(n) do
     Enum.into 2..n, []
   end
+
+  def finder(list, primes \\ [])
+
+  def finder([], primes), do: Enum.reverse(primes)
+
+  def finder([ h | t ] =_list , primes) do
+    foo = for num <- t, rem(num, h) != 0, do: num
+    finder(foo, [ h | primes])
+  end
+
 end
