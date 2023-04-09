88d2ff919fdab6e9b4658baae8019cda70ecc31a
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-09 15:59:01 -0600

    add: refac

--- sieve/lib/sieve_finder.ex
+++ sieve/lib/sieve_finder.ex
@@ -1,7 +1,7 @@
 defmodule Sieve.Finder do
   def finder(list, primes \\ [])
 
-  def finder([], primes), do: Enum.reverse(primes)
+  def finder([], primes), do: primes  # Enum.reverse(primes)
 
   def finder([h | t] = _list, primes) do
     foo = for num <- t, rem(num, h) != 0, do: num
--- sieve/test/sieve_test.exs
+++ sieve/test/sieve_test.exs
@@ -2,58 +2,13 @@ defmodule SieveTest do
   use ExUnit.Case
 
   test "returns a list from 2 up to the given range" do
-    assert Sieve.Parser.up_to(20) == [
-             2,
-             3,
-             4,
-             5,
-             6,
-             7,
-             8,
-             9,
-             10,
-             11,
-             12,
-             13,
-             14,
-             15,
-             16,
-             17,
-             18,
-             19,
-             20
-           ]
+    assert Sieve.Parser.up_to(20) == [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
   end
 
   test "if it  receives a binary, converts to integerr" do
-    assert Sieve.Parser.up_to("20") == [
-             2,
-             3,
-             4,
-             5,
-             6,
-             7,
-             8,
-             9,
-             10,
-             11,
-             12,
-             13,
-             14,
-             15,
-             16,
-             17,
-             18,
-             19,
-             20
-           ]
-  end
+    assert Sieve.Parser.up_to("20") == [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ] end
 
   test "returns the list of prime numbers from 2 to 20" do
-    assert Sieve.Finder.finder(
-             [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-             []
-           ) ==
-             [2, 3, 5, 7, 11, 13, 17, 19]
+    assert Sieve.Finder.finder( [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], []) == [2, 3, 5, 7, 11, 13, 17, 19]
   end
 end
